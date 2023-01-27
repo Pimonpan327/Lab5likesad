2f754425f32bbeafd74f2b28c45b9c2e8386ae13
Pimonpan327 <Pimonpan01102002@gmail.com>
2023-01-27 22:15:29 +0700

    likesad

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+</script>
+
+<template>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+          <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px ; margin-left: 60px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+        <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px ; margin-left:120px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+        <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+        <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px ; margin-left: 60px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+        <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:260px ; height:520px ; margin-left: 120px">
+      <img src="../image/image2.jpg" class="card-img-top" alt="..." style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Dog</h5>
+        <p class="card-text">A dog is a domestic mammal of the family Canidae and the order Carnivora. Its scientific name is Canis lupus familiaris.</p>
+        <div>
+        <LikeSad></LikeSad><small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
---
+++ src/components/LikeSad.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+const like = ref(0);
+const sad = ref(0);
+
+function likeClick(){
+  like.value+=1;
+}
+function SadClick(){
+  sad.value+=1;
+}
+
+</script>
+
+<template>
+    <div>
+    <a href="#" class="btn btn-primary " @click="likeClick()"><i class="bi bi-hand-thumbs-up"></i></a>{{like}}
+    <a href="#" class="btn btn-primary " @click="SadClick()"><i class="bi bi-emoji-frown-fill"></i></a>{{sad}}
+    </div>
+  </template>
+  <style scoped> </style>
